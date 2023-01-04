87d7714a6519857d94a634c03593c08037da78ac
Andrei Fadeev <fdv95@mail.ru>
2023-01-04 18:13:50 +0300

    PP_2.1.3_Commit_1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean==bean2);
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getMessage());
+        System.out.println(catBean1==catBean2);
     }
 }
